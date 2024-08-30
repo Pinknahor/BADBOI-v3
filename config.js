60f3194af373bd6426d4872374eedb1c338883ae
Pinknahor <142265516+Pinknahor@users.noreply.github.com>
2024-08-30 03:15:53 +0000

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ global.location = '1' //Ganti Pake Location Panel Lu
 //===========================//
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['2348140825959']
-global.ownMain = '2348140825959'
+global.owner = ['2250707802590']
+global.ownMain = '2250707802590'
 global.NamaOwner = '𝓑𝓐𝓓𝓑𝓞𝓘' //gausah diganti 
 global.sessionName = 'badboisession'
 global.connect = true // Ubah Ke False Jika Ingin Menggunakan Qr Code
@@ -46,7 +46,7 @@ global.listr = ` │⬡ 1GB Cpu: 50% - 3.000/bln
  
 //===========================//
 
-global.country = `234`
+global.country = `225`
 global.system = {
 gmail: `vinorvanderly@gmail.com`,
 }
